bfc54475b9fdb087ecf3fdfc7751afbb446bf7c2
Your Name <shubhamsaurabhk@gmail.com>
2023-04-29 21:12:37 +0530

    added admin and shop routes

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
-
 app.use(shoproutes)
+app.use((req,res,next)=>{  
+res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3001);
